7e336417998b6ad08d809eef9e283730b8c35cb4
PedroMolon <pedroh.molonr@gmail.com>
2023-08-23 18:12:26 -0300

    desafios-javascript

---
+++ desafio-javascript/script.js
@@ -0,0 +1,47 @@
+//1
+alert("Hello Wordl!");
+
+//2
+let n1, n2;
+alert(`A soma entre ${n1} + ${n2} = ${n1 + n2}`);
+
+//3
+let a = prompt("Digite algo");
+if(typeof(x) == Number) {
+  alert("A váriavel x é um Number!");
+} else {
+  alert("A váriavel não é um Number!")
+}
+
+//4
+let b = prompt("Digite algo");
+if(typeof(x) == String) {
+  alert("A váriavel x é uma String!");
+} else {
+  alert("A váriavel não é uma String!")
+}
+
+//5
+let c = prompt("Digite algo");
+if(typeof(x) == Boolean) {
+  alert("A váriavel x é um Boolena!");
+} else {
+  alert("A váriavel não é um Boolean!")
+}
+
+//6
+alert(`A subtração entre ${n1} - ${n2} = ${n1 - n2}`);
+
+//7
+alert(`A multiplicação entre ${n1} * ${n2} = ${n1 * n2}`);
+
+//8
+alert(`A divisão entre ${n1} / ${n2} = ${n1 / n2}`);
+
+//9 - 10
+let n = prompt("Digite um número:");
+if(Number(n % 2 == 0)) {
+  alert("É um número par!");
+} else {
+  alert("É um número ímpar!");
+}
